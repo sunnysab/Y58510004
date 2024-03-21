68fa639ff0173e988bbbaed2b38083a9d2ccf756
sunnysab <i@sunnysab.cn>
2024-03-21 19:38:46 +0800

    Add display_size function to generate readable file size.

--- util.cpp
+++ util.cpp
@@ -5,9 +5,14 @@
 
 #include <chrono>
 #include <sstream>
+#include <cmath>
+#include <array>
 #include "util.h"
 
-
+/// Calculate execution time of a lambda function.
+/// \tparam Func
+/// \param lambda: the lambda function to be executed and measured.
+/// \return a pair of result of lambda function and execution time in microseconds.
 template<typename Func>
 auto calc_execution_time(Func lambda) -> std::pair<decltype(std::declval<Func>()()), long long> {
     auto start = std::chrono::high_resolution_clock::now();
@@ -19,7 +24,7 @@ auto calc_execution_time(Func lambda) -> std::pair<decltype(std::declval<Func>()
 }
 
 
-
+/// Convert time in human-readable format.
 auto display_time(long long microseconds) -> std::string {
     std::ostringstream stream;
 
@@ -49,4 +54,27 @@ auto display_time(long long microseconds) -> std::string {
     }
 
     return stream.str();
+}
+
+
+
+/// Convert file size in human-readable format.
+auto display_size(long long bytes) -> std::string {
+    static const std::array<std::string, 7> suffixes = {"B", "KB", "MB", "GB", "TB", "PB", "EB"};
+    std::string formattedSize;
+    std::ostringstream stream;
+
+    if (bytes == 0) {
+        return "0 B";
+    }
+
+    int i = static_cast<int>(std::log(bytes) / std::log(1024));
+    // 防止越界
+    if (i >= suffixes.size()) i = suffixes.size() - 1;
+
+    double count = bytes / std::pow(1024, i);
+    stream.precision(2);
+    stream << std::fixed << count << " " << suffixes[i];
+    formattedSize = stream.str();
+    return formattedSize;
 }
--- util.h
+++ util.h
@@ -8,10 +8,14 @@
 #include <utility>
 #include <string>
 
+/// Calculate execution time of a lambda function.
 template<typename Func>
 auto calc_execution_time(Func lambda) -> std::pair<decltype(std::declval<Func>()()), long long>;
 
+/// Convert time in human-readable format.
 auto display_time(long long microseconds) -> std::string;
 
+/// Convert file size in human-readable format.
+auto display_size(long long bytes) -> std::string;
 
 #endif //PARALLEL_UTIL_H
