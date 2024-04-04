a4094e6dce6a3a1ac5f9c3b36478b12977ab7543
sunnysab <i@sunnysab.cn>
2024-04-04 15:23:47 +0800

    Add search_with_openmp_simd.

--- main.cpp
+++ main.cpp
@@ -24,17 +24,6 @@ struct Task {
 };
 
 
-auto do_task(Task  &task, const uint8_t *base, const char *pattern) -> std::vector<size_t> {
-    auto [_offset, _size] = task;
-    auto result = kmp_search(reinterpret_cast<const char *>(base + _offset), _size, pattern, strlen(pattern));
-
-    // kmp_search only returns the offset to the pattern from the start of the block, not the base address.
-    for (auto &r: result) {
-        r += _offset;
-    }
-    return result;
-}
-
 auto search_with_single_thread(const uint8_t* p, size_t total_length, const char *pattern) -> std::vector<size_t> {
     std::cout << "search_with_single_thread has been called." << std::endl;
 
@@ -63,7 +52,7 @@ auto search_with_single_thread_simd(const uint8_t* p, size_t total_length, const
 auto search_with_openmp(const uint8_t* p, size_t total_length, const char *pattern) -> std::vector<size_t> {
     std::cout << "search_with_openmp has been called." << std::endl;
 
-    auto processor_count = 4;
+    auto processor_count = 2;
     auto task_size = total_length / processor_count;
     std::vector<Task> tasks(processor_count);
 
@@ -71,7 +60,7 @@ auto search_with_openmp(const uint8_t* p, size_t total_length, const char *patte
     // Assume that total size is 395, we split it into 4 tasks. And the length to the pattern is 5.
     //  |__100__|__100__|__100__|__95__|
     // tasks are: [0, 100), [96, 200), [196, 300), [296, 395]
-    auto addr = p;
+    auto base_addr = p;
     auto file_len = total_length;
     auto pattern_len = strlen(pattern);
     std::generate(tasks.begin(), tasks.end(), [&, i = 0]() mutable {
@@ -92,7 +81,14 @@ auto search_with_openmp(const uint8_t* p, size_t total_length, const char *patte
         auto index = omp_get_thread_num();
         auto task = tasks[index];
 
-        mid_result[index] = do_task(task, addr, pattern);
+        auto [_offset, _size] = task;
+        auto result = kmp_search(reinterpret_cast<const char *>(base_addr + _offset), _size, pattern, strlen(pattern));
+
+        // kmp_search only returns the offset to the pattern from the start of the block, not the base address.
+        for (auto &r: result) {
+            r += _offset;
+        }
+        mid_result[index] = std::move(result);
     };
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -107,6 +103,59 @@ auto search_with_openmp(const uint8_t* p, size_t total_length, const char *patte
     return result;
 }
 
+auto search_with_openmp_simd(const uint8_t* p, size_t total_length, const char *pattern) -> std::vector<size_t> {
+    std::cout << "search_with_openmp_simd has been called." << std::endl;
+
+    auto processor_count = 2;
+    auto task_size = total_length / processor_count;
+    std::vector<Task> tasks(processor_count);
+
+    // Generate tasks.
+    // Assume that total size is 395, we split it into 4 tasks. And the length to the pattern is 5.
+    //  |__100__|__100__|__100__|__95__|
+    // tasks are: [0, 100), [96, 200), [196, 300), [296, 395]
+    auto base_addr = p;
+    auto file_len = total_length;
+    auto pattern_len = strlen(pattern);
+    std::generate(tasks.begin(), tasks.end(), [&, i = 0]() mutable {
+        auto start = i == 0 ? 0: (i * task_size - (pattern_len - 1));
+        auto real_size = i == task_size - 1 ? std::min(task_size, file_len - i * task_size) : task_size;
+        real_size += pattern_len;
+        i++;
+        return Task(start, real_size);
+    });
+
+    std::cout << "run algorithm in " << processor_count << " threads." << std::endl;
+
+    auto mid_result = std::vector<std::vector<size_t>>(processor_count);
+    auto start = std::chrono::high_resolution_clock::now();
+    // Assign tasks to threads.
+#pragma omp parallel num_threads(processor_count)
+    {
+        auto index = omp_get_thread_num();
+        auto task = tasks[index];
+
+        auto [_offset, _size] = task;
+        auto result = simd_search(reinterpret_cast<const char *>(base_addr + _offset), _size, pattern, strlen(pattern));
+
+        // kmp_search only returns the offset to the pattern from the start of the block, not the base address.
+        for (auto &r: result) {
+            r += _offset;
+        }
+        mid_result[index] = std::move(result);
+    };
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << "task finished, costs " << std::dec << duration << " microseconds (" << display_time(duration) << ")" << std::endl;
+
+    auto result = std::vector<size_t>();
+    for (auto &r: mid_result) {
+        result.insert(result.end(), r.begin(), r.end());
+    }
+
+    return result;
+}
 
 auto search_in_file(const char *file, const char *pattern) {
     FileMapper  f(file);
@@ -141,19 +190,22 @@ auto check_print_result(const uint8_t *text, size_t text_len, const char *patter
 auto do_test_in_memory(const int size, const char *pattern, const int count) {
     auto p = generate_test_data(size, pattern, count);
 
-    auto result1 = search_with_openmp(p, size, pattern);
+    auto result1 = search_with_single_thread(p, size, pattern);
     check_print_result(p, size, pattern, result1);
 
-    auto result2 = search_with_single_thread(p, size, pattern);
+    auto result2 = search_with_single_thread_simd(p, size, pattern);
     check_print_result(p, size, pattern, result2);
 
-    auto result3 = search_with_single_thread_simd(p, size, pattern);
+    auto result3 = search_with_openmp(p, size, pattern);
     check_print_result(p, size, pattern, result3);
+
+    auto result4 = search_with_openmp_simd(p, size, pattern);
+    check_print_result(p, size, pattern, result4);
 
     delete[] p;
 }
 
 int main() {
-    do_test_in_memory(1024 * 1024 * 1024, "PATTERN", 5);
+    do_test_in_memory(2048 * 1024 * 1024, "PATTERN", 5);
     return 0;
 }
